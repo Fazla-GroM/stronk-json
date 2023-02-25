77ce10ec73f0673be745cd7b510e8577465c14a5
Sasa Fazlic <fazlic.sasa@gmail.com>
2023-02-25 12:41:16 +0100

    feat: project setup

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import solid from 'solid-start/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    plugins: [solid(), vanillaExtractPlugin({ identifiers: 'short' })]
+})
